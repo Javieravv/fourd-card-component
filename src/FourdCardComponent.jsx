d08b5efb88d238ef4d6aea54e2d9b7140bdcdb66
Javier Vargas - Legion <javieravargasestudiante@gmail.com>
2023-03-25 18:18:33 -0500

    Mejora problema animación

--- src/FourdCardComponent.jsx
+++ src/FourdCardComponent.jsx
@@ -11,17 +11,17 @@ export const FourdCardComponent = () => {
             <p className="paragraph-card">Monitors activity to identify project roadblocks</p>
             <img src="/images/icon-supervisor.svg" alt="" />
           </article>
-          <article className="card-team animate__animated animate__backInLeft">
+          <article className="card-team animate__animated animate__backInDown">
             <h3>Team Builder</h3>
             <p className="paragraph-card">Scans our talent network to create the optimal team for your project</p>
             <img src="/images/icon-team-builder.svg" alt="" />
           </article>
-          <article className="card-karma animate__animated animate__backInLeft">
+          <article className="card-karma animate__animated animate__backInRight">
             <h3>Karma</h3>
             <p className="paragraph-card">Regularly evaluates our talent to ensure quality</p>
             <img src="/images/icon-karma.svg" alt="" />
           </article>
-          <article className="card-calculator animate__animated animate__backInLeft">
+          <article className="card-calculator animate__animated animate__backInUp">
             <h3>Calculator</h3>
             <p className="paragraph-card">Uses data from past projects to provide better delivery estimates</p>
             <img src="/images/icon-calculator.svg" alt="" />
